5586ea37b299de228ad75b55024a61a2ea977b42
vinothn14 <vinothn14@gmail.com>
2022-07-29 13:45:11 +0530

    update server.js

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+app.use(express.static(__dirname+'/dist/angular-clever'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/angular-clever/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
